cf64429a7654abcfcd639c99f57aea3e4c975156
Thakur Bhanu <onlyforworkandstuff@gmail.com>
2020-06-27 23:29:13 +0530

    BETA

---
+++ OLD/test2.py
@@ -0,0 +1,10 @@
+from UBUNTU import runFunctions
+
+#
+# pyautogui.hotkey('super', 'pagedown')
+
+# pyautogui.hotkey('winleft', 'pagedown')
+
+# pyautogui.hotkey('ctrlleft', 'leftalt', 't')
+
+runFunctions.forFiles()
